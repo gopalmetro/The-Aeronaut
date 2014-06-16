5238f2e5718761525f5992c4de93fad4e3f393a2
David Wert <djw2yw@virginia.edu>
2014-06-16 16:23:23 -0400

    Adds animation backbone

--- Assets/Scripts/Behaviors/Player/Controls.cs
+++ Assets/Scripts/Behaviors/Player/Controls.cs
@@ -2,6 +2,12 @@
 using System.Collections;
 
 public class Controls : MonoBehaviour {
+
+    private Animator animator;
+
+    void Start() {
+        animator = this.GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -12,7 +18,10 @@ public class Controls : MonoBehaviour {
 
     void mouseControls() {
         var screenPoint = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Input.GetMouseButtonDown(0) && this.GetComponent<Jump>().isGrounded()) { this.GetComponent<Jump>().playerJump(); }
+        if (Input.GetMouseButtonDown(0) && this.GetComponent<Jump>().isGrounded()) { 
+            this.GetComponent<Jump>().playerJump();
+            animator.SetInteger("Direction", 1);
+        }
         if (screenPoint.origin.x < transform.position.x) { this.GetComponent<Jump>().xAxisMvmtLeft(); }
         if (screenPoint.origin.x > transform.position.x) { this.GetComponent<Jump>().xAxisMvmtRight(); }
     }
@@ -20,7 +29,10 @@ public class Controls : MonoBehaviour {
     void iOSControls() {
         foreach (Touch touch in Input.touches) {
             var screenPoint = Camera.main.ScreenPointToRay(touch.position);
-            if (touch.phase == TouchPhase.Began) { this.GetComponent<Jump>().playerJump(); }
+            if (touch.phase == TouchPhase.Began) { 
+                this.GetComponent<Jump>().playerJump();
+                animator.SetInteger("Direction", 1);
+            }
             if (screenPoint.origin.x < transform.position.x) { this.GetComponent<Jump>().xAxisMvmtLeft(); }
             if (screenPoint.origin.x > transform.position.x) { this.GetComponent<Jump>().xAxisMvmtRight(); }
         }
@@ -38,7 +50,10 @@ public class Controls : MonoBehaviour {
     }
 
     void keyboardControls() {
-        if (Input.GetButtonDown("Jump")) { this.GetComponent<Jump>().playerJump(); }
+        if (Input.GetButtonDown("Jump")) { 
+            this.GetComponent<Jump>().playerJump();
+            animator.SetInteger("Direction", 1);
+        }
         if (Input.GetKey("left")) { this.GetComponent<Jump>().xAxisMvmtLeft(); }
         if (Input.GetKey("right")) { this.GetComponent<Jump>().xAxisMvmtRight(); }
     }
