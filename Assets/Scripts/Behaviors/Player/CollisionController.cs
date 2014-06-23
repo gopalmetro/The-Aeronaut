21c41b8fe27f7f25c16759e6cd58e0c776e7ea03
Chielo Zimmerman <chielo@coshx.com>
2014-06-23 19:34:32 -0400

    adds back player-balloon collision detection using notification system

--- Assets/Scripts/Behaviors/Player/CollisionController.cs
+++ Assets/Scripts/Behaviors/Player/CollisionController.cs
@@ -19,7 +19,30 @@ public class CollisionController : MonoBehaviour {
 	bool check = true;
 
 	void FixedUpdate () {
+		checkIfOnBalloon ();
 		checkIfOutOfBounds ();
+	}
+
+	void checkIfOnBalloon () {
+		float playerSize = this.renderer.bounds.size.y;
+		Vector3 position1 = transform.position;
+		Vector3 position2 = transform.position;
+		position2.x = position2.x + playerSize;
+		position2.y = position2.y - playerSize / 2;
+		
+		Collider2D[] hits = Physics2D.OverlapAreaAll (new Vector2 (position1.x, position1.y), new Vector2 (position2.x, position2.y));
+		Notification collision = new Notification (NotificationType.OnBalloonPlayerCollision, "Balloon Collided!");
+		
+		int i = 0;
+		while (i < hits.Length) {
+			Collider2D hit = hits [i];
+			if (hit != null) {
+				if (hit.tag == "platform") {
+					NotificationCenter.defaultCenter.postNotification (collision);
+				}
+			}
+			i++;
+		}
 	}
 
 	void checkIfOutOfBounds () {
