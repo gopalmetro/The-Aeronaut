675f340ebe133a8543b97b448319e31246e3a6fa
David Wert <djw2yw@virginia.edu>
2014-06-30 12:58:20 -0400

    Adds moveable eye to intro screen

--- Assets/Scripts/Behaviors/Buttons/startbutton.cs
+++ Assets/Scripts/Behaviors/Buttons/startbutton.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class startbutton : MonoBehaviour {
 
-    public Texture2D startbuttonimage;
+    public Texture2D buttonimage;
     public GUISkin GUIskin; 
 
     void OnGUI() {
         GUI.skin = GUIskin;
-        if (GUI.Button(new Rect(Screen.width / 2 + 50, Screen.height / 2 + 250, startbuttonimage.width +100, startbuttonimage.height +100), startbuttonimage)) {
+        if (GUI.Button(new Rect(Screen.width / 2 + 50, Screen.height / 2 + 250, buttonimage.width +100, buttonimage.height +100), buttonimage)) {
             Application.LoadLevel("stage1");
         }
     }
---
+++ Assets/Scripts/Behaviors/Eye/Eye.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Eye : MonoBehaviour {
+
+    private Vector3 mousePosition;
+    private GameObject eye;
+    private float initZ;
+	// Use this for initialization
+	void Start () {
+        eye = GameObject.Find("Eye");
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        initZ = this.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = new Vector3(mousePosition.x, mousePosition.y, initZ);
+
+	    // first, find the distance from the center of the eye to the target
+        Vector3 distanceToTarget = mousePosition - eye.transform.position;
+ 
+        // clamp the distance so it never exceeds the size of the eyeball
+        distanceToTarget = Vector3.ClampMagnitude( distanceToTarget, eye.GetComponent<CircleCollider2D>().radius - this.GetComponent<CircleCollider2D>().radius);
+ 
+        // place the pupil at the desired position relative to the eyeball
+        Vector3 finalPupilPosition  = eye.transform.position + distanceToTarget;
+        this.transform.position = finalPupilPosition;
+	}
+}
