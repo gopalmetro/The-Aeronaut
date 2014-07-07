07a6ba851496b2532fd5164fee5a9f0caa4643bd
David Wert <djw2yw@virginia.edu>
2014-07-07 12:56:21 -0400

    Makes death dependent on timer rather than alpha

--- Assets/Scripts/Behaviors/DeathBehavior.cs
+++ Assets/Scripts/Behaviors/DeathBehavior.cs
@@ -5,9 +5,12 @@ public class DeathBehavior : MonoBehaviour {
 
     private GameObject player;
     private bool gameOver = false;
+    private float deathTimer = 1.5f;
+    private float originalDeathTimer;
 
 	// Use this for initialization
 	void Start () {
+        originalDeathTimer = deathTimer;
         player = GameObject.Find("player");
         Color textureColor = this.guiTexture.color;
         textureColor.a = 0;
@@ -19,18 +22,18 @@ public class DeathBehavior : MonoBehaviour {
 	void Update () {
         Color textureColor = this.guiTexture.color;
         if (player.rigidbody2D.velocity.y  < -15) {
-            //textureColor.a = -(player.rigidbody2D.velocity.y) / 60;
+            deathTimer -= Time.deltaTime;
             float alpha = Mathf.Lerp(textureColor.a, 1, 0.9f * Time.deltaTime);
             textureColor.a = alpha;
             guiTexture.color = textureColor;
-            if (textureColor.a > .6) {
+            if (deathTimer <= 0) {
                 gameOver = true;
                 Notification Death = new Notification(NotificationType.Death);
                 NotificationCenter.defaultCenter.postNotification(Death);
             }
-            //Debug.Log(textureColor.a);
         }
         else if (!gameOver) {
+            deathTimer = originalDeathTimer;
             textureColor.a = 0;
             guiTexture.color = textureColor;
         }
