4a15c509823e7c976a12bf4db0a152f624d6d8e1
Chielo Zimmerman <chielo@coshx.com>
2014-07-08 13:40:52 -0400

    catches  notifications containing destroyed background game objects

--- Assets/Scripts/Behaviors/Background Objects/BackgroundController.cs
+++ Assets/Scripts/Behaviors/Background Objects/BackgroundController.cs
@@ -4,97 +4,97 @@ using System.Collections.Generic;
 
 public class BackgroundController : MonoBehaviour {
 
-    public GameObject backgroundPrefab;
-    private List<Sprite> backgrounds;
-    private List<GameObject> backgroundParts;
-    private int heightToBeginTransition = 5;
-    private int heightToBeginSpaceTransition = 5 + 8;
+	public GameObject backgroundPrefab;
+	private List<Sprite> backgrounds;
+	private List<GameObject> backgroundParts;
+	private int heightToBeginTransition = 5;
+	private int heightToBeginSpaceTransition = 5 + 8;
 
-	// Use this for initialization
 	void Start () {
-        NotificationCenter.defaultCenter.addListener(onNotification, NotificationType.BackgroundObjectNotification);
-        backgrounds = new List<Sprite>();
-        backgroundParts = new List<GameObject>();
-        LoadImages(backgrounds);
+		NotificationCenter.defaultCenter.addListener (onNotification, NotificationType.BackgroundObjectNotification);
+		backgrounds = new List<Sprite> ();
+		backgroundParts = new List<GameObject> ();
+	
+		LoadImages (backgrounds);
 
-        for (int i = 0; i <= 2; i++) {
-			GameObject newBackground = Instantiate(backgroundPrefab) as GameObject;
-		    newBackground.GetComponent<Background>().setSprite(backgrounds[0]);
-		    newBackground.transform.parent = this.transform;
-            newBackground.GetComponent<Background>().setIndex(0);
-            newBackground.GetComponent<Background>().setHeight(i);
-            newBackground.GetComponent<Background>().setID((char)(i + 70));
-            newBackground.GetComponent<Background>().setColliderBounds();
-            backgroundParts.Add(newBackground);
-        }
+		for (int i = 0; i <= 2; i++) {
+			GameObject newBackground = Instantiate (backgroundPrefab) as GameObject;
+			newBackground.GetComponent<Background> ().setSprite (backgrounds [0]);
+			newBackground.transform.parent = this.transform;
+			newBackground.GetComponent<Background> ().setIndex (0);
+			newBackground.GetComponent<Background> ().setHeight (i);
+			newBackground.GetComponent<Background> ().setID ((char)(i + 70));
+			newBackground.GetComponent<Background> ().setColliderBounds ();
+			backgroundParts.Add (newBackground);
+		}
 	}
 
-    private void onNotification(Notification note) {
+	private void onNotification (Notification note) {
+		BackgroundNotification current = (BackgroundNotification)note;
 
-        BackgroundNotification current = (BackgroundNotification)note;
-        Background currentBackground = current.gameObj.GetComponent<Background>();
-        List<GameObject> tempBackgroundParts = new List<GameObject>();
-        int currentIndex = currentBackground.getIndex();
-        int currentHeight = currentBackground.getHeight();
-        foreach (GameObject a in backgroundParts) {
-            
-			if (a.GetComponent<Background>().id != currentBackground.id){
-                a.GetComponent<Background>().isCenter = false;
-                tempBackgroundParts.Add(a);
-            }
-        }
+		try {
+			Background currentBackground = current.gameObj.GetComponent<Background> ();
+			List<GameObject> tempBackgroundParts = new List<GameObject> ();
+			int currentIndex = currentBackground.getIndex ();
+			int currentHeight = currentBackground.getHeight ();
 
-        if (currentHeight < heightToBeginTransition) {
-            tempBackgroundParts[1].GetComponent<Background>().setIndex(0);
-            tempBackgroundParts[1].GetComponent<Background>().setSprite(backgrounds[0]);
-            tempBackgroundParts[1].GetComponent<Background>().setHeight(currentHeight + 1);
-            tempBackgroundParts[0].GetComponent<Background>().setIndex(0);
-            tempBackgroundParts[0].GetComponent<Background>().setSprite(backgrounds[0]);
-            tempBackgroundParts[0].GetComponent<Background>().setHeight(currentHeight - 1);
-        }
-        else if (currentHeight  >= heightToBeginTransition && currentHeight < heightToBeginSpaceTransition - 1) {
+			foreach (GameObject a in backgroundParts) {
+            		
+				if (a.GetComponent<Background> ().id != currentBackground.id) {
+					a.GetComponent<Background> ().isCenter = false;
+					tempBackgroundParts.Add (a);
+				}
+			}
 
-            if (currentIndex - 1 < 0) {
-                tempBackgroundParts[0].GetComponent<Background>().setIndex(currentIndex);
-                tempBackgroundParts[0].GetComponent<Background>().setSprite(backgrounds[currentIndex]);
-                tempBackgroundParts[0].GetComponent<Background>().setHeight(currentHeight - 1);
-            }
-            else {
-                tempBackgroundParts[0].GetComponent<Background>().setIndex(currentIndex - 1);
-                tempBackgroundParts[0].GetComponent<Background>().setSprite(backgrounds[currentIndex - 1]);
-                tempBackgroundParts[0].GetComponent<Background>().setHeight(currentHeight - 1);
-            }
+			if (currentHeight < heightToBeginTransition) {
+				tempBackgroundParts [1].GetComponent<Background> ().setIndex (0);
+				tempBackgroundParts [1].GetComponent<Background> ().setSprite (backgrounds [0]);
+				tempBackgroundParts [1].GetComponent<Background> ().setHeight (currentHeight + 1);
+				tempBackgroundParts [0].GetComponent<Background> ().setIndex (0);
+				tempBackgroundParts [0].GetComponent<Background> ().setSprite (backgrounds [0]);
+				tempBackgroundParts [0].GetComponent<Background> ().setHeight (currentHeight - 1);
+			} else if (currentHeight >= heightToBeginTransition && currentHeight < heightToBeginSpaceTransition - 1) {
 
-            if (currentIndex + 1 < backgrounds.Count)
-            {
-                tempBackgroundParts[1].GetComponent<Background>().setIndex(currentIndex + 1);
-                tempBackgroundParts[1].GetComponent<Background>().setSprite(backgrounds[currentIndex + 1]);
-                tempBackgroundParts[1].GetComponent<Background>().setHeight(currentHeight + 1);
-            }
-            else
-            {
-                tempBackgroundParts[1].GetComponent<Background>().setIndex(currentIndex);
-                tempBackgroundParts[1].GetComponent<Background>().setSprite(backgrounds[currentIndex]);
-                tempBackgroundParts[1].GetComponent<Background>().setHeight(currentHeight + 1);
-            }
+				if (currentIndex - 1 < 0) {
+					tempBackgroundParts [0].GetComponent<Background> ().setIndex (currentIndex);
+					tempBackgroundParts [0].GetComponent<Background> ().setSprite (backgrounds [currentIndex]);
+					tempBackgroundParts [0].GetComponent<Background> ().setHeight (currentHeight - 1);
+				} else {
+					tempBackgroundParts [0].GetComponent<Background> ().setIndex (currentIndex - 1);
+					tempBackgroundParts [0].GetComponent<Background> ().setSprite (backgrounds [currentIndex - 1]);
+					tempBackgroundParts [0].GetComponent<Background> ().setHeight (currentHeight - 1);
+				}
 
-        }
-        else {
-            tempBackgroundParts[1].GetComponent<Background>().setIndex(backgrounds.Count - 1);
-            tempBackgroundParts[1].GetComponent<Background>().setSprite(backgrounds[backgrounds.Count - 1]);
-            tempBackgroundParts[1].GetComponent<Background>().setHeight(currentHeight + 1);
-            tempBackgroundParts[0].GetComponent<Background>().setIndex(backgrounds.Count - 1);
-            tempBackgroundParts[0].GetComponent<Background>().setSprite(backgrounds[backgrounds.Count - 1]);
-            tempBackgroundParts[0].GetComponent<Background>().setHeight(currentHeight - 1);
-        }
-    }
+				if (currentIndex + 1 < backgrounds.Count) {
+					tempBackgroundParts [1].GetComponent<Background> ().setIndex (currentIndex + 1);
+					tempBackgroundParts [1].GetComponent<Background> ().setSprite (backgrounds [currentIndex + 1]);
+					tempBackgroundParts [1].GetComponent<Background> ().setHeight (currentHeight + 1);
+				} else {
+					tempBackgroundParts [1].GetComponent<Background> ().setIndex (currentIndex);
+					tempBackgroundParts [1].GetComponent<Background> ().setSprite (backgrounds [currentIndex]);
+					tempBackgroundParts [1].GetComponent<Background> ().setHeight (currentHeight + 1);
+				}
 
+			} else {
+				tempBackgroundParts [1].GetComponent<Background> ().setIndex (backgrounds.Count - 1);
+				tempBackgroundParts [1].GetComponent<Background> ().setSprite (backgrounds [backgrounds.Count - 1]);
+				tempBackgroundParts [1].GetComponent<Background> ().setHeight (currentHeight + 1);
+				tempBackgroundParts [0].GetComponent<Background> ().setIndex (backgrounds.Count - 1);
+				tempBackgroundParts [0].GetComponent<Background> ().setSprite (backgrounds [backgrounds.Count - 1]);
+				tempBackgroundParts [0].GetComponent<Background> ().setHeight (currentHeight - 1);
+			}
 
-    private void LoadImages(List<Sprite> backgrounds) {
-        for (int i = 1; i < 09; i++) {
-            string texture = "Textures/backgrounds/0" + i;
-            Sprite texTmp = (Sprite)Resources.Load(texture, typeof(Sprite));
-            backgrounds.Add(texTmp);
-        }
-    }
+		} catch (MissingReferenceException e) {
+			//stale notification
+		}
+
+	}
+
+	private void LoadImages (List<Sprite> backgrounds) {
+		for (int i = 1; i < 09; i++) {
+			string texture = "Textures/backgrounds/0" + i;
+			Sprite texTmp = (Sprite)Resources.Load (texture, typeof(Sprite));
+			backgrounds.Add (texTmp);
+		}
+	}
 }
--- Assets/Scripts/Behaviors/DeathBehavior.cs
+++ Assets/Scripts/Behaviors/DeathBehavior.cs
@@ -3,47 +3,50 @@ using System.Collections;
 
 public class DeathBehavior : MonoBehaviour {
 
-    private GameObject player;
-    private bool gameOver = false;
-    private float deathTimer = 1.5f;
-    private float originalDeathTimer;
+	private GameObject player;
+	private bool gameOver = false;
+	private float deathTimer = 1.5f;
+	private float originalDeathTimer;
 
 	// Use this for initialization
 	void Start () {
-        originalDeathTimer = deathTimer;
-        player = GameObject.Find("player");
-        Color textureColor = this.guiTexture.color;
-        textureColor.a = 0;
-        guiTexture.color = textureColor;
-        NotificationCenter.defaultCenter.addListener(onDeath, NotificationType.Death);
+		originalDeathTimer = deathTimer;
+		player = GameObject.Find ("player");
+		Color textureColor = this.guiTexture.color;
+		textureColor.a = 0;
+		guiTexture.color = textureColor;
+		NotificationCenter.defaultCenter.addListener (onDeath, NotificationType.Death);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Color textureColor = this.guiTexture.color;
-        if (player.rigidbody2D.velocity.y  < -15) {
-            deathTimer -= Time.deltaTime;
-            float alpha = Mathf.Lerp(textureColor.a, 1, 0.9f * Time.deltaTime);
-            textureColor.a = alpha;
-            guiTexture.color = textureColor;
-            if (deathTimer <= 0) {
-                gameOver = true;
-                Notification Death = new Notification(NotificationType.Death);
-                NotificationCenter.defaultCenter.postNotification(Death);
-            }
-        }
-        else if (!gameOver) {
-            deathTimer = originalDeathTimer;
-            textureColor.a = 0;
-            guiTexture.color = textureColor;
-        }
+		Color textureColor = this.guiTexture.color;
+		if (player.rigidbody2D.velocity.y < -15) {
+			deathTimer -= Time.deltaTime;
+			float alpha = Mathf.Lerp (textureColor.a, 1, 0.9f * Time.deltaTime);
+			textureColor.a = alpha;
+			guiTexture.color = textureColor;
+			if (deathTimer <= 0) {
+				gameOver = true;
+				Notification Death = new Notification (NotificationType.Death);
+				NotificationCenter.defaultCenter.postNotification (Death);
+			}
+		} else if (!gameOver) {
+			deathTimer = originalDeathTimer;
+			textureColor.a = 0;
+			guiTexture.color = textureColor;
+		}
 
 	}
 
-    private void onDeath(Notification Note)  {
-        Color textureColor = this.guiTexture.color;
-        this.gameOver = true;
-        textureColor.a = 1;
-        guiTexture.color = textureColor;
-    }
+	private void onDeath (Notification Note) {
+		try {
+			Color textureColor = this.guiTexture.color;
+			this.gameOver = true;
+			textureColor.a = 1;
+			guiTexture.color = textureColor;
+		} catch (MissingReferenceException e) {
+			//stale notification
+		}
+	}
 }
