811ce73b848879c91a7ebb2a462080ae5f8289e2
coshx labs <info@coshx.com>
2014-07-10 11:25:29 -0400

    First version of GUI

--- Assets/Scripts/Behaviors/Buttons/VolumeSlider.cs
+++ Assets/Scripts/Behaviors/Buttons/VolumeSlider.cs
@@ -8,19 +8,21 @@ public class VolumeSlider : MonoBehaviour {
     public Texture2D background = null;
     public Texture2D button = null;
     public string controller = null;
-    public float posY = 0f;
+	public int negOrPos = 0;
+    private float offset = Screen.width/8;
     private GUIStyle slider;
     private GUIStyle thumbnail;
 
     void Start() {
+		offset = negOrPos * offset;
         slider = new GUIStyle(GUISkin.horizontalSlider) {
-                fixedHeight = background.height,
-                fixedWidth  = background.width / 2,
+                fixedHeight = Screen.width/16,
+                fixedWidth  = Screen.width / 2,
 
             };
         thumbnail = new GUIStyle(GUISkin.horizontalSliderThumb) {
-                fixedHeight = button.height - button.width / 4,
-                fixedWidth = button.width - button.width / 4,
+                fixedHeight = Screen.width/8,
+                fixedWidth = Screen.width/8 ,
                 
             };
     }
@@ -36,8 +38,8 @@ public class VolumeSlider : MonoBehaviour {
         GUI.skin = GUISkin;
 
         GUI.skin.horizontalScrollbar.fixedWidth = 200;
-        hSliderValue = GUI.HorizontalSlider (new Rect (Screen.width / 2 - background.width / 4, Screen.height / 4 - posY, 
-            background.width / 2 , background.height), hSliderValue, 0, 1.0f, slider, thumbnail);
+        hSliderValue = GUI.HorizontalSlider (new Rect (Screen.width / 2 - slider.fixedWidth/2, Screen.height / 4 - offset, 
+            Screen.width / 2 , background.height), hSliderValue, 0, 1.0f, slider, thumbnail);
         if (GameObject.Find("Sounds") != null) {
             GameObject aud = GameObject.Find(controller);
             aud.GetComponent<AudioSource>().volume = hSliderValue;
